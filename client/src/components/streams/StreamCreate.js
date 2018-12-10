acab795c12d2b0b278e5b6ae63d42ca9d5b6160b
Nellie K <noircir@users.noreply.github.com>
2018-12-10 09:32:21 -0500

    Finish StreamCreate

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,55 +1,74 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import{ connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component {
 
+    // flash error message when the form field is left empty
     renderError( { error, touched}) {
         if (touched && error) {
             return (
                 <div className="ui error message">
-                    <div className="header">
                         {error}
-                    </div>
-                
                 </div>
             );
         }
     }
 
-    renderInput = ({ input, label, meta }) => {
-        // console.log(meta.touched);
-        console.log(this);
+    renderField = ({ 
+        input, 
+        label,
+        type,
+        meta }) => {
 
+        // the field itself turns pink if a user moved away from it
         const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+
+        // {...input} means taking all the usual input props (value, onChange, etc) 
+        // and add them to the <input /> element
+        // It replaces:
+        // <input onChange={formProps.input.onChange} value={formProps.input.value} />
         return (
             <div className={className}>
                 <label>{label}</label>
-                <input {...input} autoComplete="off" />
+                <input {...input} autoComplete="off" type={type}/>
                 {this.renderError(meta)}
             </div>
         )
     }
 
-    onSubmit(formValues) {
-        console.log(formValues)
+    // Every time Submit button is pressed, the field values will be first validated,
+    // and if everything is OK, createStream action creator will be called 
+    // and passed formValues, and an axios call will be sent out to our API server
+    // to 'post' a new stream. 
+
+    onSubmit = (formValues) => {
+        // console.log(formValues) // Two values were passed: title and description
+        this.props.createNewStream(formValues); // mapDispatchToProps
     }
 
     render() {
         // console.log(this.props)
+        // this.props.handleSubmit() is provided by the redux form.
+        // We pass it our own this.onSubmit with all values of our form (formValues).
         return (
             <form 
                 onSubmit={this.props.handleSubmit(this.onSubmit)} 
+                // Without class 'error' the error won't show up on the page (SemanticUI default)
                 className="ui form error"
             >
                 <Field 
                     name="title" 
-                    component={this.renderInput} 
+                    component={this.renderField} 
                     label="Enter Title"
+                    type="text"
                 />
                 <Field 
                     name="description" 
-                    component={this.renderInput} 
+                    component={this.renderField} 
                     label="Enter Description" 
+                    type="text"
                 />
                 <button className="ui button primary">Submit</button>
             </form>
@@ -60,16 +79,22 @@ class StreamCreate extends React.Component {
 const validate = (formValues) => {
     const errors = {};
     if (!formValues.title) {
-        errors.title = 'The title field cannot be empty.';
+        errors.title = 'Title must be specified.';
     }
     if (!formValues.description) {
-        errors.description = 'The description field cannot be empty.';
+        errors.description = 'Description must be specified.';
     }
     // console.log(errors);
+
+    // The errors are passed to 'renderField' function.
     return errors;
 }
 
-export default reduxForm({
+// To wire up both 'connect' and 'reduxForm' to our class StreamCreate,
+// separate into two parts:
+const formWrapped = reduxForm({
     form: 'streamCreate',
     validate
 })(StreamCreate);
+
+export default connect(null, { createNewStream: createStream })(formWrapped);
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,6 +2,9 @@ import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
 
+// The form reducer is already provided by redux-form, 
+// we just need to wire it up into our store.
+
 export default combineReducers ({
     auth: authReducer,
     form: formReducer
