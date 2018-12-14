1e522ecc6fc26acd92127e4ae81abcaf4e06c650
Nellie K <noircir@users.noreply.github.com>
2018-12-13 19:36:26 -0500

    Finish Stream Show - REST app complete

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -45,7 +45,9 @@ class StreamList extends React.Component {
                     {this.renderAdmin(stream)}
                     <i className="large middle aligned icon camera" />
                     <div className="content">
-                        <h4>{stream.title}</h4>
+                        <Link to={`/streams/${stream.id}`} className="header">
+                            {stream.title}
+                        </Link>
                         <div className="description">
                             {stream.description}
                         </div>
